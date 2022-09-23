ac71b5eafa3627225514e29e943d0a57a2864c92
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2022-09-23 16:24:16 +0900

    Update Student.java

--- java/hk220923/src/staticex/Student.java
+++ java/hk220923/src/staticex/Student.java
@@ -2,7 +2,7 @@ package staticex;
 
 public class Student {
 	//정적 변수
-	int serialNum = 1000;// 클래스 변수
+	static int serialNum = 1000;// 클래스 변수
 	// 모든 객체가 공유하는 데이터를 만들어 사용
 	int studentId;
 	String studentName;
