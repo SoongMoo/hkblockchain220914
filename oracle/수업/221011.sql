7803ac64661724e3d7e761f93a670d2d1099709e
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2022-10-11 13:31:16 +0900

    Add files via upload

--- oracle/수업/221011.sql
+++ oracle/수업/221011.sql
@@ -122,4 +122,99 @@ select first_name, last_name, hire_date,
 from hr.employees e join  hr.departments d
 USING (DEPARTMENT_ID);
 --- using절에서 사용한 컬럼은 select 절에 테이블별칭을 주지 않는다.
--- `
+--
+
+--- T-SQL Join , Ansi - Join, NATURAL JOIN , using절
+-- 각 직원의 직원번호, 직무번호, 부서번호, 부서명
+-- 이 때 20,30,50, 80인 부서만 출력
+select employee_id, job_id, e.department_id , department_name
+from hr.employees e, hr.departments d
+where e.department_id = d.department_id
+and e.department_id in (20,30,50, 80);
+
+select employee_id, job_id, e.department_id , department_name
+from hr.employees e join hr.departments d
+on e.department_id = d.department_id
+and e.department_id in (20,30,50, 80);
+
+select employee_id, job_id, department_id , department_name
+from hr.employees  NATURAL join hr.departments 
+where department_id in (20,30,50, 80);
+
+select employee_id, job_id, department_id , department_name
+from hr.employees e join hr.departments d
+using (department_id)
+where department_id in (20,30,50, 80);
+
+select * from hr.locations;
+--- 부서번호, 부서명, 우편번호, 주소를 출력할 때 우편번호가 1400만 출력
+--T-SQL Join
+select department_id, department_name, 
+        l.location_id, street_address
+from hr.departments d, hr.locations l
+where d.location_id = l.location_id
+and l.location_id = 1400;
+-- Ansi - Join
+select department_id, department_name, 
+        l.location_id, street_address
+from hr.departments d join hr.locations l
+on d.location_id = l.location_id
+where l.location_id = 1400;
+
+-- NATURAL 
+select department_id, department_name, 
+       location_id, street_address
+from hr.departments NATURAL join hr.locations;
+
+-- using 
+select department_id, department_name, 
+       location_id, street_address
+from hr.departments join hr.locations
+using(location_id);
+
+--- 3way join : 다중 조인 
+-- SH_CLERK을 직무를 가진 사원의 사원번호 이름 직무 부서번호 출력을 하는데 
+-- SH_CLERK의 직무 내용을 확인하고.
+-- SH_CLERK의 직무를 가지고 있는 사원의 부서명 출력하시오.
+select employee_id, first_name, job_id, department_id
+from hr.employees
+where job_id = 'SH_CLERK';
+
+select department_id, department_name
+from hr.departments
+where department_id = 50;
+
+select job_id, job_title
+from hr.jobs
+where job_id = 'SH_CLERK';
+--- T-SQL Join
+select employee_id, first_name, -- employees
+         d.department_id, department_name, -- departments
+         j.job_id, job_title --- jobs
+from hr.jobs j, hr.employees e , hr.departments d
+where j.job_id = e.job_id
+and e.department_id = d.department_id
+and e.job_id = 'SH_CLERK';
+
+select employee_id, first_name, -- employees
+         d.department_id, department_name, -- departments
+         j.job_id, job_title --- jobs
+from hr.jobs j join hr.employees e 
+on j.job_id = e.job_id join  hr.departments d
+on e.department_id = d.department_id
+where e.job_id = 'SH_CLERK';
+
+select employee_id, first_name, -- employees
+         department_id, department_name, -- departments
+            job_id, job_title --- jobs
+from hr.jobs j NATURAL join hr.employees e 
+               NATURAL join  hr.departments d
+where e.job_id = 'SH_CLERK';
+               
+select employee_id, first_name, -- employees
+         department_id, department_name, -- departments
+            job_id, job_title --- jobs
+from hr.jobs join hr.employees
+using (job_id) join  hr.departments 
+using(department_id)
+where job_id = 'SH_CLERK';
