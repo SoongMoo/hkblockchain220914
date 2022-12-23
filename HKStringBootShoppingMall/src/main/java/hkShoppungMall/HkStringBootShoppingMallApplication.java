0f2c0f48e8bd95492ad96f76a0611a84a1b7b817
user <user@DESKTOP-3Q11Q03>
2022-12-23 10:35:41 +0900

    spring boot start

---
+++ HKStringBootShoppingMall/src/main/java/hkShoppungMall/HkStringBootShoppingMallApplication.java
@@ -0,0 +1,12 @@
+package hkShoppungMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkStringBootShoppingMallApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HkStringBootShoppingMallApplication.class, args);
+	}
+
+}
