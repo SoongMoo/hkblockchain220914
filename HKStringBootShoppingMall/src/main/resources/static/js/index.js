f433e7ab44c8a420dc0ab0e75d2c271a2142843c
user <user@DESKTOP-3Q11Q03>
2023-03-10 09:10:57 +0900

    자바스크립트 암호화

--- HKStringBootShoppingMall/src/main/resources/static/js/index.js
+++ HKStringBootShoppingMall/src/main/resources/static/js/index.js
@@ -1,6 +1,30 @@
 window.addEventListener("load", function() {
 	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+	//encrypt();
 });
+
+function encrypt() {
+	const message = "4d45c9a55174c00a794a290af534488045f8dda96e99ecf7622c920606e614a3";
+    const password = "hello blockchain";
+    const salt = CryptoJS.lib.WordArray.random(128 / 8);
+    const key = CryptoJS.PBKDF2(password, salt, { keySize: 256 / 32, iterations: 1000 });
+    const iv = CryptoJS.lib.WordArray.random(128 / 8);
+    const ciphertext = CryptoJS.AES.encrypt(message, key, { iv: iv });
+    const encrypted = salt.toString() + iv.toString() + ciphertext.toString();
+    document.write(encrypted);
+}
+// 복호화
+function decrypt(message,pass) {
+ 	const encrypted = message;
+    const password = pass;
+    const salt = CryptoJS.enc.Hex.parse(encrypted.substr(0, 32));
+    const iv = CryptoJS.enc.Hex.parse(encrypted.substr(32, 32));
+    const ciphertext = encrypted.substring(64);
+    const key = CryptoJS.PBKDF2(password, salt, { keySize: 256 / 32, iterations: 1000 });
+    const decrypted = CryptoJS.AES.decrypt({ ciphertext: CryptoJS.enc.Base64.parse(ciphertext) }, key, { iv: iv }).toString(CryptoJS.enc.Utf8);
+    //$('#decrypted-message-input').val(decrypted);
+  	return decrypted;
+}
 
 /// 자바스크립트를 이용해서 파일 읽어오기
 function onFileSelected(event) {
@@ -8,7 +32,8 @@ function onFileSelected(event) {
 	const reader = new FileReader();
 	reader.onload = function(event) {
 		console.log("파일 내용 :" + event.target.result);
-		const privateKey = event.target.result;
+		const file = event.target.result;
+		const privateKey = decrypt(file, "hello blockchain");
 		/// 지갑
 		const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 		const address = account.address;
